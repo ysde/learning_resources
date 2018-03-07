e0211d23915e4eeb0d57be15e3e1196c3c1cd34e
ysde <ysde108@gmail.com>
2018-03-07 18:56:17 +0800

    update gitlab-runner deploy script

--- my_docker_run_script/gitlab-runner/start.sh
+++ my_docker_run_script/gitlab-runner/start.sh
@@ -1,22 +1,38 @@
 #!/bin/bash
-
-# https://docs.gitlab.com/runner/install/docker.html
-
+GITLAB_MASTER_IP=xxxx.xxxx.xxx.xxx
 CONTAINER_NAME=gitlab-runner
-CONFIG_FOLDER=/srv/gitlab-runner/config/config.toml
+CONFIG_FOLDER=/opt/gitlab-runner/config
+
+REGISTER_TOKEN="xxxxxxxxxxxxxxx"
+
+# If you don't have a public domain, you need to set --docker-extra-hosts for your
+GITLAB_DOMAIN="gitlab.xxxxx.xxxx"
+
+status=`systemctl show --property ActiveState docker | cut -d= -f2`
+
+while [ "$status" != "active" ]
+do
+	echo "sleep 5 sec to wait docker service up"
+	sleep 5
+	status=`systemctl show --property ActiveState docker | cut -d= -f2`
+done
 
 docker rm -f $CONTAINER_NAME
+rm -rf ${CONFIG_FOLDER}/*
 
 docker run --net=host -d --name $CONTAINER_NAME --restart always \
   -v ${CONFIG_FOLDER}:/etc/gitlab-runner \
   -v /var/run/docker.sock:/var/run/docker.sock \
   gitlab/gitlab-runner:latest
 
-sleep 2
+echo "gitlab-runner container started"
+echo "register gitlab-runner"
+docker exec ${CONTAINER_NAME} gitlab-runner register -n --url http://${GITLAB_MASTER_IP} --registration-token ${REGISTER_TOKEN} --executor docker --docker-image docker:latest --docker-extra-hosts "${GITLAB_DOMAIN}:${GITLAB_MASTER_IP}" --docker-privileged --docker-volumes "/var/run/docker.sock:/var/run/docker.sock" "/cache"
+echo "finish registering gitlab-runner."
 
-docker exec -it $CONTAINER_NAME gitlab-runner register
+sleep 5
+echo "modify gitlab-runner global setting: concurrent to 10"
+sed -i -e 's/concurrent = 1/concurrent = 10/g' ${CONFIG_FOLDER}/config.toml
 
-# set extra host, otherwise gitlab-runner doesn' know YOUR_DOMAIN even you set the /etc/hosts
-# sed -i '/\[runners.docker\]/a \ \ \ \ extra_hosts = ["YOUR_DOMAIN:ip_address"]' ${CONFIG_FOLDER}/config.toml
-
-docker restart $CONTAINER_NAME
+echo "restart gitlab-runner"
+docker restart ${CONTAINER_NAME}
